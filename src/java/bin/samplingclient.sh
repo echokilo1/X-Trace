14b2065d3c2e22e91936f75bbd54058694ac3213
Raja Sambasivan <rajas@andrew.cmu.edu>
2011-12-01 05:10:30 -0500

    Fix a few typos and add log4j as a dependency.

--- src/java/bin/samplingclient.sh
+++ src/java/bin/samplingclient.sh
@@ -3,7 +3,7 @@
 BASE=`dirname ${0}`/..
 
 if [ $# != 2 ]; then
-    echo -e "Usage: samplingclient.sh [hostname] [samplingpercenage]";
+    echo -e "Usage: samplingclient.sh [hostname] [samplingpercentage]";
     exit
 fi
 
@@ -12,6 +12,6 @@ HST=$1;
 # The sampling percentage
 SAMPLE_PERCENTAGE=$2;
       
-CLASSPATH=~/.m2/repository/commons-logging/commons-logging/1.0.3/commons-logging-1.0.3.jar:$BASE/target/xtrace-2.2-11_2011.jar
-echo $SAMPLE_PERCENTAGE
+CLASSPATH=~/.m2/repository/commons-logging/commons-logging/1.0.3/commons-logging-1.0.3.jar:~/.m2/repository/log4j/log4j/1.2.14/log4j-1.2.14.jar:$BASE/target/xtrace-2.2-11_2011.jar
+
 java -cp $CLASSPATH edu.berkeley.xtrace.samplingserver.SamplingPercentageClient $HST $SAMPLE_PERCENTAGE
